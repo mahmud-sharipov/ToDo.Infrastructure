a8df30e4e269a98d4e5593dfd6a28290781f4aef
Mahmud Sharipov <mahmud@silkroadprofessionals.com>
2023-03-15 00:06:49 +0500

    update

--- Build/client-build.ps1
+++ Build/client-build.ps1
@@ -4,4 +4,5 @@ Param(
 
 docker build -f "C:\Repos\ToDo\src\Web.Server\Dockerfile" --force-rm -t todoclient:$v --target final "C:\Repos\Todo\src" 
 docker image tag todoclient:$v mahmudsharipov/todoclient:$v
+docker image tag todoclient:$v mahmudsharipov/todoclient:latest
 docker image push mahmudsharipov/todoclient:$v
--- Build/server-build.ps1
+++ Build/server-build.ps1
@@ -4,4 +4,5 @@ Param(
 
 docker build -f "C:\Repos\ToDo\src\API\Dockerfile" --force-rm -t todoserver:$v --target final "C:\Repos\Todo\src" 
 docker image tag todoserver:$v mahmudsharipov/todoserver:$v
+docker image tag todoserver:$v mahmudsharipov/todoserver:latest
 docker image push mahmudsharipov/todoserver:$v
